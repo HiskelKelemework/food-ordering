69964d3a8fa6176cd8d558519e0f4bf8a8546a48
Hiskel Kelemework <hiskelkelemework6@gmail.com>
2022-06-22 15:43:03 +0300

    WIP

--- infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/config/data/KafkaConsumerConfigData.java
+++ infrastructure/kafka/kafka-config-data/src/main/java/com/food/ordering/system/config/data/KafkaConsumerConfigData.java
@@ -1,6 +1,5 @@
 package com.food.ordering.system.config.data;
 
-import com.sun.org.apache.xpath.internal.operations.Bool;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
